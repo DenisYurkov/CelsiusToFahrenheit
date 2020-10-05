7fc384a521d6889f98277db17dd39a690ec93aa1
Denis Yurkov <denisdeveloper136@gmail.com>
2020-10-05 21:17:53 +0200

    Add project!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write celsius: ");
+
+            int celsius = Convert.ToInt32(Console.ReadLine());
+            int fahrenheit = celsius * 9 / 5 + 32;
+
+            Console.WriteLine("{0} °C: {1} °F", celsius, fahrenheit);
+            Console.ReadLine();
+        }
+    }
+}
